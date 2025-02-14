58d3afe71ee36b9a8eb0949993e9d458f9450f61
LAB_C_REAL <labc@continental.edu.pe>
2025-02-13 22:47:53 -0500

    oño

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const { neon } = require("@neondatabase/serverless");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const sql = neon(process.env.DATABASE_URL);
+
+ // Ruta principal
+app.get('/', async (req, res) => {
+try {
+    const result = await sql`SELECT * FROM tareas`; // Consulta todos los registros de la tabla
+    res.json(result); // Devuelve los datos en formato JSON
+    } catch (error) {
+    res.status(500).send("Error al obtener los datos: " + error.message);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
